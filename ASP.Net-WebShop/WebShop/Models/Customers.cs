608834597f25e3954ca5caa87a3f322611a026e6
Psanyi89 <pleskosandor@gmail.com>
2018-11-25 00:59:06 +0100

    My First commint 2018.11.25

---
+++ ASP.Net-WebShop/WebShop/Models/Customers.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using CompareAttribute = System.ComponentModel.DataAnnotations.CompareAttribute;
+
+namespace WebShop.Models
+{
+    public class Customers
+    {
+
+        [Display(Name ="Username")]
+        [StringLength(100,ErrorMessage ="Your username is {0} long and it's need to be at least {1} long.",MinimumLength =5)]
+        [RegularExpression("^[a-zA-Z0-9]+$",ErrorMessage ="Username can contains only alphabets and numbers.")]
+        [Required(ErrorMessage ="Username required")]
+        [Remote("IsUsernameSigned", "Account", HttpMethod = "POST", ErrorMessage = "Username already in use.")]
+        public string Username { get; set; }
+        [Display(Name = "First Name")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "First name can contains only alphabets.")]
+        [Required(ErrorMessage = "First name required")]
+        public string Firstname { get; set; }
+        [Display(Name = "Last Name")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Last name can contains only alphabets.")]
+        [Required(ErrorMessage = "Last name required")]
+        public string Lastname { get; set; }
+        [Display(Name ="Phonenumber")]
+        [DataType(DataType.PhoneNumber,ErrorMessage ="Please enter a valid phonenumber")]
+        [Required(ErrorMessage = "Phonenumber required")]
+        public string Phonenumber { get; set; }
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Please enter a valid email address")]
+        [Required(ErrorMessage = "Email address required")]
+        [Remote("IsEmailSigned", "Account", HttpMethod = "POST", ErrorMessage = "Email already in use.")]
+        public string Email { get; set; }
+        [Display(Name = "Confirm Email Address")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Please enter a valid email address")]
+        [Compare("Email",ErrorMessage ="Emails don't match.")]
+        [Required(ErrorMessage ="Confirm Email address required")]
+        public string Confirmemail { get; set; }
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password required")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}",
+        ErrorMessage = @"Password must contain at least 1 lowercase, an uppercase letter,
+a number, one special character, and must be eight characters or longer.")]
+        public string Passwd { get; set; }
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Passwd",ErrorMessage ="Passwords don't match")]
+        [Required(ErrorMessage ="Confirm password required")]
+        public string Confirmpasswd { get; set; }
+        [DisplayFormat(DataFormatString ="{0:dd MMM yyyy}")]
+        [Display(Name ="Date of Birth")]
+        [DataType(DataType.Date,ErrorMessage ="Invalid date format")]
+        [Required(ErrorMessage ="Date of Birth required")]
+        public DateTime Birthdate { get; set; }
+        [Display(Name = "Country")]
+        [Required(ErrorMessage = "Country required")]
+        public string Country { get; set; }
+        [Display(Name = "State")]
+        [Required(ErrorMessage ="Province required")]
+        public string States { get; set; }
+        [Display(Name ="City")]
+        [Required(ErrorMessage ="City required")]
+        public string Settlement { get; set; }
+        [Display(Name ="PostCode")]
+        [DataType(DataType.PostalCode)]
+        [Required(ErrorMessage ="PostCode required")]
+        public string Zipcode { get; set; }
+        [Display(Name ="Address")]
+        [Required(ErrorMessage ="Address required")]
+        public string Addresses { get; set; }
+        
+
+    }
+}
