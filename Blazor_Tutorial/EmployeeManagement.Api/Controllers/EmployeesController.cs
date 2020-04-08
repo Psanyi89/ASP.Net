161daef46edb0b6d731ae890aedab85555bb8a56
Psanyi89 <pleskosandor@gmail.com>
2020-04-09 00:53:05 +0200

    Blazor tutorial initial commit

---
+++ Blazor_Tutorial/EmployeeManagement.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,126 @@
+﻿using EmployeeManagement.Api.Models;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController :ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            this._employeeRepository = employeeRepository;
+        }
+
+        // GET: api/employees
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+
+            try
+            {
+                return Ok(await _employeeRepository.GetEmployees());
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError
+                    , "Error retrieving data from the database");
+
+            }
+        }
+
+        // GET: api/employees/id
+       [HttpGet("{id:int}")]
+       
+       public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            try
+            {
+                var result = await _employeeRepository.GetEmployee(id);
+                if (result==null)
+                {
+                    return NotFound();
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError
+                   , "Error retrieving data from the database");
+            }
+        }
+
+        //POST: api/employees
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {           
+            try
+            {
+                if (employee==null)
+                {
+                    return BadRequest();
+                }
+
+                var emp = await _employeeRepository.GetEmployeeByEmail(employee.Email);
+                if (emp!=null)
+                {
+                    ModelState.AddModelError("email", "Employee email already in use");
+                    return BadRequest(ModelState);
+                }
+            var createdEmployee= await _employeeRepository.AddEmployee(employee);
+                return CreatedAtAction(nameof(GetEmployee),new { id=createdEmployee.EmployeeId},createdEmployee);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError
+                 , "Error retrieving data from the database");
+            }
+        }
+
+        //PUT: api/employees
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Employee>> UpdateEmployee(int id, Employee employee)
+        {
+            try
+            {
+                if (id!=employee.EmployeeId)
+                {
+                    return BadRequest("Employee ID mistmatch");
+                }
+                var employeeToUpdate = await _employeeRepository.GetEmployee(id);
+                if (employeeToUpdate==null)
+                {
+                    return NotFound($"Employee with Id = {id} not found");
+                }
+
+               return await _employeeRepository.UpdateEmployee(employee);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError
+               , "Error updating data");
+            }
+          
+        }
+    
+        // DELETE: api/employees/id
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<bool>> DeleteEmployee(int id)
+        {
+          return await  _employeeRepository.DeleteEmployee(id);
+        }
+    }
+}
---
+++ Blazor_Tutorial/EmployeeManagement.Api/Models/EmployeeRepository.cs
@@ -0,0 +1,77 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+        }
+        public async Task<Employee> AddEmployee(Employee employee)
+        {
+            var result = await _appDbContext.Employees.AddAsync(employee);
+            await _appDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<bool> DeleteEmployee(int employeeId)
+        {
+            var result = await _appDbContext.Employees.FirstOrDefaultAsync(
+                e => e.EmployeeId == employeeId);
+            if (result!=null)
+            {
+                _appDbContext.Employees.Remove(result);
+                await _appDbContext.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<Employee> GetEmployee(int employeeId)
+        {
+           return await _appDbContext.Employees.FirstOrDefaultAsync(
+                 e => e.EmployeeId == employeeId);
+           
+        }
+
+        public async Task<Employee> GetEmployeeByEmail(string email)
+        {
+            return await _appDbContext.Employees.FirstOrDefaultAsync(
+                  e => e.Email == email);
+        }
+
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await _appDbContext.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> UpdateEmployee(Employee employee)
+        {
+            var result= await _appDbContext.Employees.FirstOrDefaultAsync(
+                 e => e.EmployeeId == employee.EmployeeId);
+            if (result!=null)
+            {
+                result.FirstName = employee.FirstName;
+                result.LastName = employee.LastName;
+                result.Department = employee.Department;
+                result.DateOfBirth = employee.DateOfBirth;
+                result.Email = employee.Email;
+                result.Gender = employee.Gender;
+                result.PhotoPath = employee.PhotoPath;
+                
+                await _appDbContext.SaveChangesAsync();
+                
+                return result;
+            }
+            return null;
+        }
+    }
+}
